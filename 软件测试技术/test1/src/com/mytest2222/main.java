31ec59fcdb3e075713339f30fc8bf5c82dc81351
shuxin233 <62021004+shuxin233@users.noreply.github.com>
2020-11-25 13:23:58 +0800

    Update main.java

--- 软件测试技术/test1/src/com/mytest2222/main.java
+++ 软件测试技术/test1/src/com/mytest2222/main.java
@@ -5,15 +5,18 @@ import java.util.Calendar;
 
 public class main {
     public static void main(String[] args) {
-        //唯一输入
-        //People people1=new People("张三","34220119510218061X","机关事业","副处长","退休",false);
-        People people1=new People("张三","34220119610218062X","机关事业","普通职工",false);
 
+
+
+        //People people1=new People("张三","34220119510218061X","机关事业","副处长","退休",false);
+        People people1=new People("张三","34220119550218062X","机关事业","普通职工",false,false);
         //注：positon的判断依据为 是否为普通职工
-        People entirePeople=entirePeople(people1);//根据身份证提取people完整信息
+
+        People entirePeople=entirePeople(people1);//根据身份证完善people信息
         System.out.println(entirePeople);
 
-        double[] a=function(entirePeople,"二级医院",450,2);//a[0]为可报销多少钱，a[1]为应付多少钱
+        double[] a=function(entirePeople,"一级医院",60000,1);//a[0]为可报销多少钱，a[1]为应付多少钱
+
         System.out.println("可报销"+a[0]+"元，"+"应付"+a[1]+"元。");
 
     }
@@ -28,53 +31,61 @@ public class main {
         entirePeople.setAge(age);
 
         int sex=Integer.valueOf(id.substring(16,17));//获取身份证号的倒数第二位
-        if(sex==1||age==3||age==5||age==7||age==9){//判断男女
-            entirePeople.setSex("男");
-            if(people.getNature().equals("机关事业")){
-                if(age>=60){
-                    entirePeople.setState("退休");
-                }else {
-                    entirePeople.setState("在职");
-                }
-            }else {//在企业单位
-                if(age>=55){
-                    entirePeople.setState("退休");
-                }else {
-                    entirePeople.setState("在职");
-                }
-            }
+
+        if(people.isEarly()){
+            entirePeople.setState("退休");
         }else {
-            entirePeople.setSex("女");
-            if(people.getNature().equals("机关事业")){
-                if(people.getPosition().equals("普通职工")){
-                    if(age>=55){
-                        entirePeople.setState("退休");
-                    }else {
-                        entirePeople.setState("在职");
-                    }
-                }else {
+            if(sex==1||age==3||age==5||age==7||age==9){//判断男女
+                entirePeople.setSex("男");
+                if(people.getNature().equals("机关事业")){
                     if(age>=60){
                         entirePeople.setState("退休");
                     }else {
                         entirePeople.setState("在职");
                     }
-                }
-            }else {//在企业单位
-                if(people.getPosition().equals("普通职工")){
-                    if(age>=50){
-                        entirePeople.setState("退休");
-                    }else {
-                        entirePeople.setState("在职");
-                    }
-                }else {
+                }else {//在企业单位
                     if(age>=55){
                         entirePeople.setState("退休");
                     }else {
                         entirePeople.setState("在职");
                     }
                 }
+            }else
+            {
+                entirePeople.setSex("女");
+                if(people.getNature().equals("机关事业")){
+                    if(people.getPosition().equals("普通职工")){
+                        if(age>=55){
+                            entirePeople.setState("退休");
+                        }else {
+                            entirePeople.setState("在职");
+                        }
+                    }else {
+                        if(age>=60){
+                            entirePeople.setState("退休");
+                        }else {
+                            entirePeople.setState("在职");
+                        }
+                    }
+                }else {//在企业单位
+                    if(people.getPosition().equals("普通职工")){
+                        if(age>=50){
+                            entirePeople.setState("退休");
+                        }else {
+                            entirePeople.setState("在职");
+                        }
+                    }else {
+                        if(age>=55){
+                            entirePeople.setState("退休");
+                        }else {
+                            entirePeople.setState("在职");
+                        }
+                    }
+                }
             }
         }
+
+
         return entirePeople;
     }
 
@@ -86,22 +97,33 @@ public class main {
                     result=0;
                 }else {
                     if(people.getState().equals("在职")){
-                            if(price<=55000){
+                            if(price<=55000+800){
                                 result=(price-800)*0.85;
                             }
-                            else {
-                                result=(price-800)*0.80;
+                            if(price<150000+800&&price>55000+800) {
+                                result=55000*0.85+(price-800-55000)*0.80;
+                            }
+                            if(price>=150000+800) {
+                                result=55000*0.85+(150000-55000)*0.80;
                             }
                     }else {//不在职
                         if(people.getAge()>=70||people.isLaomo()){//老工人或者劳模
-                            result=(price-800)*0.95;
+                            if(price<=150000+800){
+                                result=(price-800)*0.95;
+                            }else {
+                                result=150000*0.95;
+                            }
                         }else {//非老工人或劳模
-                            if(price<=55000){
+                            if(price<=55000+800){
                                 result=(price-800)*0.90;
                             }
-                            else {
-                                result=(price-800)*0.80;
+                            if(price<150000+800&&price>55000+800) {
+                                result=55000*0.90+(price-800-55000)*0.80;
                             }
+                            if(price>=150000+800) {
+                                result=55000*0.90+(150000-55000)*0.80;
+                            }
+
                         }
                     }
                 }
@@ -110,50 +132,72 @@ public class main {
                     result=0;
                 }else {
                     if(people.getState().equals("在职")){
-                        if(price<=55000){
+                        if(price<=55000+270){
                             result=(price-270)*0.85;
                         }
-                        else {
-                            result=(price-270)*0.80;
+                        if(price<150000+270&&price>55000+270) {
+                            result=55000*0.85+(price-270-55000)*0.80;
+                        }
+                        if(price>=150000+270) {
+                            result=55000*0.85+(150000-55000)*0.80;
                         }
                     }else {//不在职
                         if(people.getAge()>=70||people.isLaomo()){//老工人或者劳模
-                            result=(price-270)*0.95;
+                            if(price<=150000+270){
+                                result=(price-270)*0.95;
+                            }else {
+                                result=150000*0.95;
+                            }
                         }else {//非老工人或劳模
-                            if(price<=55000){
+                            if(price<=55000+270){
                                 result=(price-270)*0.90;
                             }
-                            else {
-                                result=(price-270)*0.80;
+                            if(price<150000+270&&price>55000+270) {
+                                result=55000*0.90+(price-270-55000)*0.80;
+                            }
+                            if(price>=150000+270) {
+                                result=55000*0.90+(150000-55000)*0.80;
                             }
                         }
                     }
                 }
-
             }
         }
+
+
         if(hospital.equals("二级医院")){
             if(time==1){//第一次
                 if(price<=1100){
                     result=0;
                 }else {
                     if(people.getState().equals("在职")){
-                        if(price<=55000){
+                        if(price<=55000+1100){
                             result=(price-1100)*0.85;
                         }
-                        else {
-                            result=(price-1100)*0.80;
+                        if(price<150000+1100&&price>55000+1100) {
+                            result=55000*0.85+(price-1100-55000)*0.80;
+                        }
+                        if(price>=150000+1100) {
+                            result=55000*0.85+(150000-55000)*0.80;
                         }
                     }else {//不在职
                         if(people.getAge()>=70||people.isLaomo()){//老工人或者劳模
-                            result=(price-1100)*0.95;
+                            if(price<=150000+1100){
+                                result=(price-1100)*0.95;
+                            }else {
+                                result=150000*0.95;
+                            }
                         }else {//非老工人或劳模
-                            if(price<=55000){
+                            if(price<=55000+1100){
                                 result=(price-1100)*0.90;
                             }
-                            else {
-                                result=(price-1100)*0.80;
+                            if(price<150000+1100&&price>55000+1100) {
+                                result=55000*0.90+(price-1100-55000)*0.80;
                             }
+                            if(price>=150000+1100) {
+                                result=55000*0.90+(150000-55000)*0.80;
+                            }
+
                         }
                     }
                 }
@@ -162,51 +206,72 @@ public class main {
                     result=0;
                 }else {
                     if(people.getState().equals("在职")){
-                        if(price<=55000){
+                        if(price<=55000+350){
                             result=(price-350)*0.85;
                         }
-                        else {
-                            result=(price-350)*0.80;
+                        if(price<150000+350&&price>55000+350) {
+                            result=55000*0.85+(price-350-55000)*0.80;
+                        }
+                        if(price>=150000+350) {
+                            result=55000*0.85+(150000-55000)*0.80;
                         }
                     }else {//不在职
                         if(people.getAge()>=70||people.isLaomo()){//老工人或者劳模
-                            result=(price-350)*0.95;
+                            if(price<=150000+350){
+                                result=(price-350)*0.95;
+                            }else {
+                                result=150000*0.95;
+                            }
                         }else {//非老工人或劳模
-                            if(price<=55000){
+                            if(price<=55000+350){
                                 result=(price-350)*0.90;
                             }
-                            else {
-                                result=(price-350)*0.80;
+                            if(price<150000+350&&price>55000+350) {
+                                result=55000*0.90+(price-350-55000)*0.80;
+                            }
+                            if(price>=150000+270) {
+                                result=55000*0.90+(150000-55000)*0.80;
                             }
                         }
                     }
                 }
-
             }
         }
-        if(hospital.equals("三级医院")){
 
+
+        if(hospital.equals("三级医院")){
             if(time==1){//第一次
                 if(price<=1700){
                     result=0;
                 }else {
                     if(people.getState().equals("在职")){
-                        if(price<=55000){
+                        if(price<=55000+1700){
                             result=(price-1700)*0.85;
                         }
-                        else {
-                            result=(price-1700)*0.80;
+                        if(price<150000+1700&&price>55000+1700) {
+                            result=55000*0.85+(price-1700-55000)*0.80;
+                        }
+                        if(price>=150000+1700) {
+                            result=55000*0.85+(150000-55000)*0.80;
                         }
                     }else {//不在职
                         if(people.getAge()>=70||people.isLaomo()){//老工人或者劳模
-                            result=(price-1700)*0.95;
+                            if(price<=150000+1700){
+                                result=(price-1700)*0.95;
+                            }else {
+                                result=150000*0.95;
+                            }
                         }else {//非老工人或劳模
-                            if(price<=55000){
+                            if(price<=55000+1700){
                                 result=(price-1700)*0.90;
                             }
-                            else {
-                                result=(price-1700)*0.80;
+                            if(price<150000+1700&&price>55000+1700) {
+                                result=55000*0.90+(price-1700-55000)*0.80;
                             }
+                            if(price>=150000+1700) {
+                                result=55000*0.90+(150000-55000)*0.80;
+                            }
+
                         }
                     }
                 }
@@ -215,32 +280,41 @@ public class main {
                     result=0;
                 }else {
                     if(people.getState().equals("在职")){
-                        if(price<=55000){
+                        if(price<=55000+500){
                             result=(price-500)*0.85;
                         }
-                        else {
-                            result=(price-500)*0.80;
+                        if(price<150000+500&&price>55000+500) {
+                            result=55000*0.85+(price-500-55000)*0.80;
+                        }
+                        if(price>=150000+500) {
+                            result=55000*0.85+(150000-55000)*0.80;
                         }
                     }else {//不在职
                         if(people.getAge()>=70||people.isLaomo()){//老工人或者劳模
-                            result=(price-500)*0.95;
+                            if(price<=150000+500){
+                                result=(price-500)*0.95;
+                            }else {
+                                result=150000*0.95;
+                            }
                         }else {//非老工人或劳模
-                            if(price<=55000){
+                            if(price<=55000+500){
                                 result=(price-500)*0.90;
                             }
-                            else {
-                                result=(price-500)*0.80;
+                            if(price<150000+500&&price>55000+500) {
+                                result=55000*0.90+(price-500-55000)*0.80;
+                            }
+                            if(price>=150000+500) {
+                                result=55000*0.90+(150000-55000)*0.80;
                             }
                         }
                     }
                 }
-
             }
         }
 
         double[] a=new double[2];
         a[0]=result;//报销
-        a[1]=price-result;//应付
+        a[1]=price-result;//应付多少钱
         return a;
     }
 }
@@ -256,26 +330,20 @@ class People{
     private String position;//职务
     private String state;//就业状态
     private boolean laomo;//是否劳模
-    private int age;
-    private String sex;
+    private int age;//年龄
+    private String sex;//性别
+    private boolean early;//是否提前退休
 
 
     public People(){};
-    public People(String name, String id, String nature, String position, String state, boolean laomo) {
-        this.name = name;
-        this.id = id;
-        this.nature = nature;
-        this.position = position;
-        this.state = state;
-        this.laomo = laomo;
-    }
 
-    public People(String name, String id, String nature, String position, boolean laomo) {
+    public People(String name, String id, String nature, String position, boolean laomo,boolean early) {
         this.name = name;
         this.id = id;
         this.nature = nature;
         this.position = position;
         this.laomo = laomo;
+        this.early=early;
     }
 
     @Override
@@ -355,10 +423,12 @@ class People{
     public void setSex(String sex) {
         this.sex = sex;
     }
+
+    public boolean isEarly() {
+        return early;
+    }
+
+    public void setEarly(boolean early) {
+        this.early = early;
+    }
 }
-
-
-
-
-
-
